41dfb5bb0e7a74434e66dcec86df52890358d2fd
soren-atkinson <sorenatkinson0905@gmail.com>
2026-04-22 14:58:00 -0600

    hello

--- ponder/meta-css.js
+++ ponder/meta-css.js
@@ -1,8 +1,30 @@
+// variables vs contant
+
 const PI = 3.14;
 let radius = 3;
+
+let area = radius * radius * PI;
+
+console.log(area);
+
+radius = 20;
+area = radius * radius * PI;
+console.log(area);
+
+radius = 30;
+area = radius * radius * PI;
+console.log(area);
+
+// type coersion
+
 const one = 1;
 const two = '2';
-                    
+let result = one * two;
+console.log(result);
+
+result = one + Number(two);
+console.log(result)
+
 
 let course = "CSE131"; //global scope
 if (true) {
