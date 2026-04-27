21e525e593f9d21a1d06bfb72fe44cb9c5afd4a4
soren-atkinson <sorenatkinson0905@gmail.com>
2026-04-27 13:58:11 -0600

    Added DOM

--- ponder/meta-css.js
+++ ponder/meta-css.js
@@ -26,12 +26,12 @@ result = one + Number(two);
 console.log(result)
 
 
-let course = "CSE131"; //global scope
-if (true) {
-    let student = "John";
-    console.log(course);  //works just fine, course is global
-    console.log(student); //works just fine, it's being accessed within the block
-}
-console.log(course); //works fine, course is global
-console.log(student); //does not work, can't access a block variable outside the block
+
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+})
+                
                     
