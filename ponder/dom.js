82c2800e834cbdbd18ca56133fccbaef9768a2d7
soren-atkinson <sorenatkinson0905@gmail.com>
2026-04-27 15:00:49 -0600

    DOM Apr 27

---
+++ ponder/dom.js
@@ -0,0 +1,30 @@
+// let title = document.querySelector("h1");
+
+// console.log(title);
+
+// title.textContent = 'Web Page Document'
+
+// document.querySelector('#topics').style.color = 'red';
+
+const topics = document.querySelector('#topics');
+
+topics.style.color = 'purple';
+
+const wrapper = document.getElementById('content');
+
+wrapper.style.backgroundColor = 'lightblue';
+
+let list = document.querySelector('.list');
+
+list.style.border = "3px solid black";
+
+let para = document.querySelector('p');
+
+para.style.fontSize = '2em';
+
+para.classList.add('background')
+
+const image = document.querySelector("img")
+
+image.setAttribute('src', 'images/new_logo.png');
+
