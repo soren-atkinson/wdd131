a5e97709463064a43a31ed23a8da903617adadcb
soren-atkinson <sorenatkinson0905@gmail.com>
2026-05-11 14:43:18 -0600

    button_works

---
+++ ponder/responsive.js
@@ -0,0 +1,12 @@
+
+const menu = document.querySelector('nav');
+
+const btn = document.querySelector('.menu-btn');
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    menu.classList.toggle('hide');
+    btn.classList.toggle('change');
+};
+
